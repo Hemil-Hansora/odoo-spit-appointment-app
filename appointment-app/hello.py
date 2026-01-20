8bc0b6e2f5f83f507796b53365c18315570ee3e6
Vrundpatel1535 <132847861+Vrundpatel153@users.noreply.github.com>
2026-01-20 20:39:11 +0530

    pullreq

--- appointment-app/hello.py
+++ appointment-app/hello.py
@@ -1,1 +1,14 @@
-print("hii hemil")
+import os
+import shutil
+
+vrund = os.path.abspath(os.path.join(os.getcwd(), ".."))
+
+hansora = "appointment-app"
+
+hemil = os.path.join(vrund, hansora)
+
+if os.path.exists(hemil) and os.path.isdir(hemil):
+    shutil.rmtree(hemil)
+    print(f"Deleted folder: {hemil}")
+else:
+    print("error")
